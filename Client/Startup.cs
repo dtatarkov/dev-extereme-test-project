0ef426e12936893a3d55e7acab4cdbb8604e7ae2
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-08-13 18:15:26 +0300

    replaced Razor pages with classic MVC structure

--- Client/Startup.cs
+++ Client/Startup.cs
@@ -49,7 +49,9 @@ namespace Client
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
