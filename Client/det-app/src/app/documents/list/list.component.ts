0cdb749c5629d257909d892ab2317d08b9c20860
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-08-14 19:06:17 +0300

    client: finished documents list component

--- Client/det-app/src/app/documents/list/list.component.ts
+++ Client/det-app/src/app/documents/list/list.component.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
+import { catchError, of } from "rxjs";
 import { EffectsContainer } from "src/app/shared/effectsContainer";
 import { TderaDocument } from "../document";
 import { DocumentsService } from "../documents.service";
+
+enum DocumentsListComponentView {
+  none = 0,
+  documents = 1,
+  message = 2
+}
 
 @Component({
   selector: 'app-documents-list',
@@ -9,20 +16,52 @@ import { DocumentsService } from "../documents.service";
   styleUrls: ['./list.component.scss']
 })
 export class DocumentsListComponent {
+  private view = DocumentsListComponentView.none;
   private effects = new EffectsContainer();
+  private isLoadingDocuments = false;
 
   documents = new Array<TderaDocument>();
 
+  get areDocumentsShown() {
+    return this.view == DocumentsListComponentView.documents
+  }
+
+  get isMessageShown() {
+    return this.view == DocumentsListComponentView.message
+  }
+
+  get isReady() {
+    return !this.isLoadingDocuments;
+  }
+
   constructor(private documentsService: DocumentsService) { }
 
-  ngOnInit() {
-    this.effects.registerFactory(() => {
-      const subscription = this.documentsService.getDocuments().subscribe(documents => {
-        this.documents = documents
-      });
+  loadDocuments() {
+    if (!this.isLoadingDocuments) {
+      this.effects.registerFactory(() => {
+        this.isLoadingDocuments = true;
 
-      return { dispose: () => subscription.unsubscribe() };
-    });    
+        const subscription = this.documentsService.getDocuments()
+          .pipe(catchError(() => of(undefined)))
+          .subscribe(documents => {
+            if (documents != undefined) {
+              this.documents = documents;
+              this.view = DocumentsListComponentView.documents;
+            } else {
+              this.view = DocumentsListComponentView.message;
+            }
+            
+            this.isLoadingDocuments = false;
+            subscription.unsubscribe();
+          });
+
+        return { dispose: () => subscription.unsubscribe() };
+      });
+    }
+  }
+
+  ngOnInit() {
+    this.loadDocuments();
   }
 
   ngOnDestroy() {
